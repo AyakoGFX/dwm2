9e6b786bdcc645eeee36051d992c7bc755f7372d
AyakoGFX <ayako.gfx@gmail.com>
2024-11-13 22:25:59 +0530

    dwm patch added auto start new version

--- config.def.h
+++ config.def.h
@@ -36,7 +36,7 @@ static const Rule rules[] = {
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "Alacritty",NULL,    NULL,           0,         0,          1,           0,        -1 },
 	{ "Volapplet",NULL,     NULL,          0,         1,          0,           1,        -1 },
-	{ "Blueman-manager",NULL,NULL,         0,         1,          0,           1,        -1 }
+	{ "Blueman-manager",NULL,NULL,         0,         1,          0,           1,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
--- config.h
+++ config.h
@@ -34,6 +34,9 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "Alacritty",NULL,    NULL,           0,         0,          1,           0,        -1 },
+	{ "Volapplet",NULL,     NULL,          0,         1,          0,           1,        -1 },
+	{ "Blueman-manager",NULL,NULL,         0,         1,          0,           1,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
