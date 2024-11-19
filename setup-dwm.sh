b16d2def82c0b62fef184927c929d0bd8cd633a5
ayako <106796873+AyakoGFX@users.noreply.github.com>
2024-11-19 23:43:38 +0530

    z

--- setup-dwm.sh
+++ setup-dwm.sh
@@ -1,6 +1,12 @@
-mkdir -p ~/.temp/
-git clone https://github.com/noctuid/tdrop.git ~/.temp/tdrop
-cd ~/.temp/tdrop
-sudo make install
+# mkdir -p ~/.temp/
+# git clone https://github.com/noctuid/tdrop.git ~/.temp/tdrop
+# cd ~/.temp/tdrop
+# sudo make install
 
-
+sudo zypper in libX11-devel libXft-devel libXinerama-devel
+cp -r .dwm ~/
+sudo cp dwm.desktop /usr/share/xsessions/
+cd slstatus/
+sudo make clean install
+cd ..
+sudo make clean install
